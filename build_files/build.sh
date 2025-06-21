e318b967ae83acea142ecce9b5b35ee3f858b74d
Nathan Rew <nrew225@gmail.com>
2025-06-21 18:40:53 -0500

    add helix and bottom

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-#dnf5 install -y 
-
+dnf5 install -y helix
 
 # Use a COPR Example:
 #
@@ -19,19 +18,21 @@ set -ouex pipefail
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 copr -y enable atim/bottom
+dnf5 install -y bottom
+dnf5 copr -y disable atim/bottom
 
 #### Example for enabling a System Unit File
 
 #systemctl enable podman.socket
 
-
+# Install 45Drives file-sharing plugin for cockpit
 CFS_TGZ_URL=$(curl --fail --retry 15 --retry-all-errors -sL \
     "https://api.github.com/repos/45Drives/cockpit-file-sharing/releases/latest" | \
     jq -r '.assets[] | select(.name? | match("cockpit-file-sharing-.*.el9.noarch.rpm$")) | .browser_download_url')
 dnf --setopt=install_weak_deps=False install -y "${CFS_TGZ_URL}"
 
-
-
+# Install 45Drives identities plugin for cockpit
 CI_TGZ_URL=$(curl --fail --retry 15 --retry-all-errors -sL \
     "https://api.github.com/repos/45Drives/cockpit-identities/releases/latest" | \
     jq -r '.assets[] | select(.name? | match("cockpit-identities-.*.el8.noarch.rpm$")) | .browser_download_url')
